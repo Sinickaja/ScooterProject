13e33f9fb2c64f497d559612b389785776422ecb
Svetlana Gres <beliakova.svetlanna@gmail.com>
2023-04-30 17:16:02 +0300

    version 2

--- test_order_by_track.py
+++ test_order_by_track.py
@@ -1,12 +1,14 @@
 import configuration
 import data
 import requests
+import sender_stand_request
 
 
-# Тест. Проверить,что код ответа равен 200
+# Тест. Проверить получение заказа по треку и что его код ответа равен 200
 def test_get_order():
-    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER)
+        return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER +"?t=" +str(sender_stand_request.track)) #str() переводит формат параметра из целочисленного в строку
 
 
 response = test_get_order()
 assert response.status_code == 200
+
